83a33fcc10a7deef0bc73082359016a904d4cf9b
Matheus <mattheusgetstudio@gmail.com>
2018-09-04 21:52:37 -0300

    Cadastro de Grupos

--- src/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RequiredIfDirective } from './required-if/required-if';
-@NgModule({
-	declarations: [RequiredIfDirective],
-	imports: [],
-	exports: [RequiredIfDirective]
-})
-export class DirectivesModule {}
